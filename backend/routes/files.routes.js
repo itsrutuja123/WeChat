09e1a189e43834d0c8bffc381de352da517d2cc7
rutujajain <121030753+itsrutuja123@users.noreply.github.com>
2025-01-11 06:58:14 -0800

    Delete backend directory

--- backend/routes/files.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadFile } = require('../controllers/fileController');
-
-// POST endpoint to upload file
-router.post('/upload', uploadFile);
-
-module.exports = router;
