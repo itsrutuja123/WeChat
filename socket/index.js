ba5b82046d6be0fbc40bea78a8a7f07af2e51a9c
itsrutuja123 <rutujajain401@gmail.com>
2025-01-11 07:08:17 -0800

    Added socket folder for real-time communication

--- socket/index.js
+++ socket/index.js
@@ -9,7 +9,7 @@ let activeUsers = [];
 io.on("connection", (socket) => {
   // add new User
   socket.on("new-user-add", (newUserId) => {
-    // if user is not added previously
+    // if user is not added previously present
     if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({ userId: newUserId, socketId: socket.id });
       console.log("New User Connected", activeUsers);
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
     // remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
     console.log("User Disconnected", activeUsers);
-    // send all active users to all users
+    // send all active users
     io.emit("get-users", activeUsers);
   });
 
